57fe0d694ef722dee2e1db50763a45bdaf2a7730
phihai91 <phihai91@gmail.com>
2017-08-28 17:39:56 +0700

    Select All Post and Details

---
+++ src/limingallery/Controllers/PostsController.cs
@@ -0,0 +1,40 @@
+﻿using limingallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace limingallery.Controllers
+{
+    public class PostsController : Controller
+    {
+        private ApplicationDbContext _context;
+        public PostsController()
+        {
+            _context = new ApplicationDbContext(); //create context
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose(); // release memory
+        }
+        // GET: Posts
+        public ActionResult Index()
+        {
+            var posts = _context.Posts.ToList();
+            return View(posts);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var post = _context.Posts.SingleOrDefault(p => p.Id == id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(post);
+        }
+    }
+}
