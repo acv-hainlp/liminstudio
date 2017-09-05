756bb52b5b2ec6c2da4fa7650d5ef6d3b5fac2dd
phihai91 <phihai91@gmail.com>
2017-09-05 19:44:21 +0700

    Like Create

---
+++ src/limingallery/Controllers/LikesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using limingallery.Models;
+using Microsoft.AspNet.Identity;
+
+namespace limingallery.Controllers
+{
+    public class LikesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public LikesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(int postId)
+        {
+            var like = new Like
+            {
+                PostId = postId,
+                CreateOn = DateTime.Now,
+                UserId = User.Identity.GetUserId(),
+            };
+
+            _context.Likes.Add(like);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index","Posts");
+        }
+    }
+}
