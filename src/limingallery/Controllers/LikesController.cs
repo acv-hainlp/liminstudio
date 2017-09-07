e46abd51f00cd09dbd50a6debb38f132f410bd60
phihai91 <phihai91@gmail.com>
2017-09-07 19:45:23 +0700

    Add Like and Unlike

--- src/limingallery/Controllers/LikesController.cs
+++ src/limingallery/Controllers/LikesController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using limingallery.Models;
 using Microsoft.AspNet.Identity;
+using System.Data.Entity;
+using System.Web.Routing;
 
 namespace limingallery.Controllers
 {
@@ -30,17 +32,41 @@ namespace limingallery.Controllers
         [HttpPost]
         public ActionResult Create(int postId)
         {
-            var like = new Like
-            {
-                PostId = postId,
-                CreateOn = DateTime.Now,
-                UserId = User.Identity.GetUserId(),
-            };
+            var currentUser = User.Identity.GetUserId();
+            var findLike = _context.Likes
+                .Where(c=>c.PostId == postId && c.UserId == currentUser)
+                .FirstOrDefault(); // if user liked post, find like is true
 
-            _context.Likes.Add(like);
+            if (findLike == null) //if user not like, insert like
+            {
+                var like = new Like
+                {
+                    PostId = postId,
+                    CreateOn = DateTime.Now,
+                    UserId = currentUser,
+                };
+
+                _context.Likes.Add(like);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index", "Posts");
+
+            }
+
+            //delete if like is exit
+            return RedirectToAction("Delete", new RouteValueDictionary(
+             new { controller = "Likes", action = "Delete", Id = findLike.Id }));
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Like like = new Like { Id = id };
+            _context.Likes.Attach(like);
+            _context.Entry(like).State = EntityState.Deleted;
+
             _context.SaveChanges();
 
-            return RedirectToAction("Index","Posts");
+            return RedirectToAction("Index", "Posts");
         }
     }
 }
